9ff2f983cb75bda005bf66ce7e100e9543117eab
Ahmed Mohamed Gomaa <99025430+AhmedGomaa15@users.noreply.github.com>
2025-02-15 20:18:55 +0200

    Dashboard with react app

---
+++ src/components/MainDash/MainDash.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MainDash.css'
+import Cards from '../Cards/Cards'
+import Table from '../Table/Table'
+
+const MainDash = () => {
+  return (
+
+    <div className="MainDash">
+        <h1>Dashboard</h1>
+        <Cards/>
+        <h2>Recrnt Orders</h2>
+        <Table/>
+    </div>
+  )
+}
+
+export default MainDash
